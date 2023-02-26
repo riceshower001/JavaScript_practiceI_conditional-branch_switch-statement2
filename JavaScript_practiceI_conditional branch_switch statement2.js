866da0e606553b68062c389b167c63a97c59ae7a
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-26 15:12:28 +0900

    Add files via upload

---
+++ JavaScript_practiceI_conditional branch_switch statement2.js
@@ -0,0 +1,17 @@
+const n = 4;
+
+switch (n) {
+  case 1:
+    console.log("Poor.");
+    break;
+  case 2:
+    console.log("Good.");
+    break;
+  case 3:
+    console.log("Excellent.");
+    break;
+  // Please add processing when none of the cases match.
+  default:
+    console.log("????");
+    break;
+}
